8e7b572475bdf897d49fdc46a0109d9125a25c2c
Ninj1zz <45872531+Ninj1zz@users.noreply.github.com>
2019-10-12 17:02:55 +0200

    4 Neue Booleans

--- spiel_2019/src/spiel_client/HitBox.java
+++ spiel_2019/src/spiel_client/HitBox.java
@@ -9,16 +9,61 @@ public class HitBox {
 	int w, h;
 	int checkDown;
 
-	
-	boolean hits(var s, var h) {
+	boolean hitOben(var s, var h) {
 		Rectangle player = new Rectangle(s.x, s.y, s.playerwidth, s.playerhight);
 		Rectangle object = new Rectangle(h.player2x, h.player2y, h.playerwidth, h.playerhight);
 
-		if (player.intersects(object)) {
+		if (player.intersects(object) && s.y<h.y) {			
 			return true;
 		} else {
 			return false;
 		}
 	}
+	
+	boolean hitUnten(var s, var h) {
+		Rectangle player = new Rectangle(s.x, s.y, s.playerwidth, s.playerhight);
+		Rectangle object = new Rectangle(h.player2x, h.player2y, h.playerwidth, h.playerhight);
+
+		if (player.intersects(object) && s.y>h.y) {			
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	boolean hitLinks(var s, var h) {
+		Rectangle player = new Rectangle(s.x, s.y, s.playerwidth, s.playerhight);
+		Rectangle object = new Rectangle(h.player2x, h.player2y, h.playerwidth, h.playerhight);
+
+		if (player.intersects(object) && s.x<h.x) {			
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	boolean hitRechts(var s, var h) {
+		Rectangle player = new Rectangle(s.x, s.y, s.playerwidth, s.playerhight);
+		Rectangle object = new Rectangle(h.player2x, h.player2y, h.playerwidth, h.playerhight);
+
+		if (player.intersects(object) && s.x>h.x) {			
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	boolean hits(var s, var h) {
+		Rectangle player = new Rectangle(s.x, s.y, s.playerwidth, s.playerhight);
+		Rectangle object = new Rectangle(h.player2x, h.player2y, h.playerwidth, h.playerhight);
+
+		if (player.intersects(object)) {			
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
 
 }
